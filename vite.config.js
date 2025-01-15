0822d6e04b82a955e03c4c17f2ad09008f87bd5e
preto960 <preto.orellana@gmail.com>
2025-01-15 16:41:14 -0400

    change properties vite config

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       cache: false,
-      // make sure to externalize Vue
-      external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build for externalized deps
-        globals: {
-          vue: 'Vue'
-        },
-        exports: "named",
+        exports: 'named',
       },
     },
   },
