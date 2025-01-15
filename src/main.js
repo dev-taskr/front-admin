52a1c5ca4b39dda77927583dd14aef8163b0aa6a
preto960 <preto.orellana@gmail.com>
2025-01-15 17:23:00 -0400

    change pages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 import App from './App.vue';
-import router from 'router';
+import router from './router/index';
 
 // Aplica el tema antes de inicializar Vue
 const savedTheme = localStorage.getItem("theme");
