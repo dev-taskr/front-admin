c6bed52b3ce70aacfb838507ed61ce5ec9c2e1f2
Taskr <nas@vbox.pro>
2025-01-15 21:07:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      'vue': '/node_modules/vue/dist/vue.esm-bundler.js'
+      '@': path.resolve(__dirname, './src')
     }
   },
-  optimizeDeps: {
-    include: ['vue']
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
